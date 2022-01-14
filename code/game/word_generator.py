663e04e4291a660035c7a736f1dd6b691e089b31
dylany <dylan_young@sfu.ca>
2022-01-13 18:44:49 -0800

    docs: Added comment

--- code/game/word_generator.py
+++ code/game/word_generator.py
@@ -2,7 +2,9 @@ import random
 from pip._vendor import requests
 
 class WordGenenator:
-
+    '''
+    Gets a random word from https://random-word-api.herokuapp.com
+    '''
     def generate_word(self):
         response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
         str = response.content.decode("utf-8")
